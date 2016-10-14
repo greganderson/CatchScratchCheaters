4d25c2bb1214d6b87dada7edc291e8b0f2bdd363
Greg Anderson <greg@infobytes.com>
2016-10-13 18:24:49 -0600

    Fixed some bugs when reading weird filenames

--- catch_cheaters.py
+++ catch_cheaters.py
@@ -19,25 +19,47 @@ if save_file in o.split('\n'):
 	print save_file + ' already exists. Please remove this file and try again.'
 	exit(1)
 
+def clean_filename(filename):
+	return "".join(filename.split())
+
+def shell_quote(filename):
+	return "'" + filename.replace("'", "'\\''") + "'"
+
+def filter_projects(x):
+	return {k:v for k,v in x.iteritems() if len(v) > 1}
 
 # Store the student's projectID as the key with a list of projects that have the same ID
 project_IDs = {}
 
+# Projects that are created using the offline editor don't contain a projectID.  Store those in a list here.
+no_project_ids = []
+
 # Store the lengths of the project as the key with a list of students with that length of project as the value
 project_lengths = {}
 
-os.system('unzip ' + sys.argv[1])
+os.system('unzip ' + sys.argv[1] + ' -d submissions/')
+
 s, o = commands.getstatusoutput('ls submissions/*.sb2')
+
+# Clean up weird filenames
+for filename in o.split('\n'):
+	os.system('mv ' + shell_quote(filename) + ' ' + clean_filename(filename))
+
+s, o = commands.getstatusoutput('ls submissions/*.sb2')
+
 for filename in o.split('\n'):
 	# The name of the directory to unzip to (just the name of the file before the .sb2)
 	student_dir = filename[:-4]
 	os.system('unzip ' + filename + ' -d ' + student_dir)
 	with open(student_dir + '/project.json', 'r') as f:
 		a = json.loads(f.read())
-		projectID = a['info']['projectID']
-		if projectID not in project_IDs:
-			project_IDs[projectID] = []
-		project_IDs[projectID].append(student_dir)
+		if 'projectID' in a['info']:
+			projectID = a['info']['projectID']
+			if projectID not in project_IDs:
+				project_IDs[projectID] = []
+			project_IDs[projectID].append(student_dir)
+		else:
+			no_project_ids.append(student_dir)
 	s, wc = commands.getstatusoutput('wc ' + student_dir + '/*')
 	total_line = wc.split('\n')
 	# Get rid of empty strings
@@ -47,13 +69,16 @@ for filename in o.split('\n'):
 		project_lengths[count] = []
 	project_lengths[count].append(student_dir)
 
-filter_projects = lambda x: {k:v for k,v in x.iteritems() if len(v) > 1}
 cheated_projects = filter_projects(project_IDs)
 suspicious_projects = filter_projects(project_lengths)
 with open(save_file, 'w') as save:
 	save.write('Projects with the same projectID:')
 	save.write('\n\n')
 	save.write(json.dumps(cheated_projects, sort_keys=True, indent=4, separators=(',', ': ')))
+	save.write('\n\n')
+	save.write('Projects with no projectID:')
+	save.write('\n\n')
+	save.write(json.dumps(no_project_ids, indent=4, separators=(',', ': ')))
 	save.write('\n\n')
 	save.write('Projects with the same length project:')
 	save.write('\n\n')
